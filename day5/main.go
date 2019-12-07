86e4f8a395c690d1fbbd4f0b84c6055030936b39
Awbrey Hughlett <awbrey.hughlett@dorianbusiness.com>
2019-12-06 22:01:06 -0600

    complete day5 part 1 and 2

--- day5/main.go
+++ day5/main.go
@@ -1,9 +1,323 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
+// OpCode ...
+type OpCode int
+
+// ParameterMode ...
+type ParameterMode int
+
+const (
+	// AddOp ...
+	AddOp OpCode = 1
+	// MultiplyOp ...
+	MultiplyOp OpCode = 2
+	// InputOp ...
+	InputOp OpCode = 3
+	// OutputOp ...
+	OutputOp OpCode = 4
+	// JumpTrue ...
+	JumpTrue OpCode = 5
+	// JumpFalse ...
+	JumpFalse OpCode = 6
+	// LessThan ...
+	LessThan OpCode = 7
+	// Equals ...
+	Equals OpCode = 8
+	// TerminateOp ...
+	TerminateOp OpCode = 99
+	// PositionMode ...
+	PositionMode ParameterMode = 0
+	// ImmediateMode ...
+	ImmediateMode ParameterMode = 1
+)
+
+// Executable ...
+type Executable interface {
+	OpCode() OpCode
+	GetValue(int, []int) int
+	GetInput(io.Reader, io.Writer, string) (int, error)
+	SetValue(int, int, []int) []int
+	SetOutput(io.Writer, string)
+	SetNext(int)
+	NextInstruction() int
+}
+
+// Instruction ...
+type Instruction struct {
+	Position   int
+	Op         OpCode
+	Parameters []Parameter
+	Modes      []ParameterMode
+	Next       *int
+}
+
+// OpCode ...
+func (i *Instruction) OpCode() OpCode {
+	return i.Op
+}
+
+// GetValue ...
+func (i *Instruction) GetValue(index int, set []int) int {
+	mode := i.Modes[index]
+	parm := i.Parameters[index]
+	switch mode {
+	case PositionMode:
+		return set[parm.Value]
+	case ImmediateMode:
+		return parm.Value
+	}
+
+	return 0
+}
+
+// GetInput ...
+func (i *Instruction) GetInput(in io.Reader, out io.Writer, message string) (int, error) {
+	reader := bufio.NewReader(in)
+	fmt.Fprintf(out, "%s\n", message)
+	text, _ := reader.ReadString('\n')
+	t := strings.TrimRightFunc(text, func(r rune) bool {
+		return unicode.IsSpace(r)
+	})
+	return strconv.Atoi(t)
+}
+
+// SetValue ...
+func (i *Instruction) SetValue(value int, index int, set []int) []int {
+	mode := i.Modes[index]
+	parm := i.Parameters[index]
+	switch mode {
+	case PositionMode:
+		set[parm.Value] = value
+	case ImmediateMode:
+		set[parm.Position] = value
+	}
+
+	return set
+}
+
+// SetOutput ...
+func (i *Instruction) SetOutput(out io.Writer, message string) {
+	fmt.Fprintf(out, "%s\n", message)
+}
+
+// SetNext ...
+func (i *Instruction) SetNext(position int) {
+	i.Next = &position
+}
+
+// NextInstruction ...
+func (i *Instruction) NextInstruction() int {
+	switch i.Op {
+	case TerminateOp:
+		return -1
+	default:
+		if i.Next != nil {
+			return *i.Next
+		}
+		return i.Position + len(i.Parameters) + 1
+	}
+}
+
+// Parameter ...
+type Parameter struct {
+	Value    int
+	Position int
+}
+
+// SplitOp ...
+func SplitOp(opcode int) (OpCode, []ParameterMode) {
+	padded := []byte(fmt.Sprintf("%6v", strconv.Itoa(opcode)))
+
+	g := strings.TrimLeftFunc(string(padded[4:]), func(r rune) bool {
+		return unicode.IsSpace(r)
+	})
+	oc64, _ := strconv.ParseInt(g, 10, 64)
+	op := int(oc64)
+
+	params := []ParameterMode{}
+	for i := 3; i >= 0; i-- {
+		p, _ := strconv.Atoi(string(padded[i]))
+		var mode ParameterMode
+		switch p {
+		case 1:
+			mode = ImmediateMode
+		case 0:
+		default:
+			mode = PositionMode
+		}
+		params = append(params, mode)
+	}
+
+	var oc OpCode
+	switch op {
+	case 1:
+		oc = AddOp
+	case 2:
+		oc = MultiplyOp
+	case 3:
+		oc = InputOp
+	case 4:
+		oc = OutputOp
+	case 5:
+		oc = JumpTrue
+	case 6:
+		oc = JumpFalse
+	case 7:
+		oc = LessThan
+	case 8:
+		oc = Equals
+	case 99:
+		oc = TerminateOp
+	default:
+		panic(fmt.Sprintf("incorrect op code: %v; for %v", op, opcode))
+	}
+
+	return oc, params
+}
+
+// GetParameters ...
+func GetParameters(quantity int, position int, set []int) []Parameter {
+	params := make([]Parameter, quantity)
+
+	for j, p := range set[position : position+len(params)] {
+		params[j] = Parameter{Value: p, Position: j + position}
+	}
+
+	return params
+}
+
+// GetInstruction ...
+func GetInstruction(i int, set []int) Instruction {
+	code, modes := SplitOp(set[i])
+
+	var params []Parameter
+	var in Instruction
+
+	switch code {
+	case AddOp, MultiplyOp, LessThan, Equals:
+		params = GetParameters(3, i+1, set)
+	case JumpTrue, JumpFalse:
+		params = GetParameters(2, i+1, set)
+	case InputOp, OutputOp:
+		params = GetParameters(1, i+1, set)
+	case TerminateOp:
+		params = []Parameter{}
+	default:
+		panic("unknown op code")
+	}
+
+	in = Instruction{
+		Position:   i,
+		Op:         code,
+		Parameters: params,
+		Modes:      modes}
+
+	return in
+}
+
+// ExecInstruction ...
+func ExecInstruction(in io.Reader, out io.Writer, ex Instruction, set []int) int {
+	switch ex.OpCode() {
+	case AddOp:
+		r := Add(ex.GetValue(0, set), ex.GetValue(1, set))
+		set = ex.SetValue(r, 2, set)
+	case MultiplyOp:
+		r := Mult(ex.GetValue(0, set), ex.GetValue(1, set))
+		set = ex.SetValue(r, 2, set)
+	case InputOp:
+		input, _ := ex.GetInput(in, out, "Enter input value: ")
+		set = ex.SetValue(input, 0, set)
+	case OutputOp:
+		ex.SetOutput(out, fmt.Sprintf("%v", ex.GetValue(0, set)))
+	case JumpTrue:
+		if ex.GetValue(0, set) != 0 {
+			ex.SetNext(ex.GetValue(1, set))
+		}
+	case JumpFalse:
+		if ex.GetValue(0, set) == 0 {
+			ex.SetNext(ex.GetValue(1, set))
+		}
+	case LessThan:
+		a := ex.GetValue(0, set)
+		b := ex.GetValue(1, set)
+		if a < b {
+			ex.SetValue(1, 2, set)
+		} else {
+			ex.SetValue(0, 2, set)
+		}
+	case Equals:
+		a := ex.GetValue(0, set)
+		b := ex.GetValue(1, set)
+		if a == b {
+			ex.SetValue(1, 2, set)
+		} else {
+			ex.SetValue(0, 2, set)
+		}
+	case TerminateOp:
+		fallthrough
+	default:
+		return ex.NextInstruction()
+	}
+
+	return ex.NextInstruction()
+}
+
+// Add ...
+func Add(i, j int) int {
+	return i + j
+}
+
+// Mult ...
+func Mult(i, j int) int {
+	return i * j
+}
+
+// ReadCodes ...
+func ReadCodes() []int {
+	codes := []int{}
+
+	file, err := os.Open("./instructions.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		values := strings.Split(scanner.Text(), ",")
+		for _, v := range values {
+			vi, _ := strconv.Atoi(v)
+			codes = append(codes, vi)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return codes
+}
+
+// Process ...
+func Process(in io.Reader, out io.Writer, position int, set []int) {
+	instruction := GetInstruction(position, set)
+	position = ExecInstruction(in, out, instruction, set)
+	if position >= 0 {
+		Process(in, out, position, set)
+	}
+}
+
 func main() {
-	println(fmt.Sprintf("value: %v", 5))
+	Process(os.Stdin, os.Stdout, 0, ReadCodes())
 }
--- day5/main_test.go
+++ day5/main_test.go
@@ -1,1 +1,222 @@
 package main
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestSplitOp(t *testing.T) {
+	inputs := []int{1002, 11101, 3, 4, 105, 1006, 7, 8, 99}
+
+	expectedOps := []OpCode{
+		MultiplyOp,
+		AddOp,
+		InputOp,
+		OutputOp,
+		JumpTrue,
+		JumpFalse,
+		LessThan,
+		Equals,
+		TerminateOp}
+
+	expectedMod := [][]ParameterMode{
+		[]ParameterMode{PositionMode, ImmediateMode, PositionMode, PositionMode},
+		[]ParameterMode{ImmediateMode, ImmediateMode, ImmediateMode, PositionMode},
+		[]ParameterMode{PositionMode, PositionMode, PositionMode, PositionMode},
+		[]ParameterMode{PositionMode, PositionMode, PositionMode, PositionMode},
+		[]ParameterMode{ImmediateMode, PositionMode, PositionMode, PositionMode},
+		[]ParameterMode{PositionMode, ImmediateMode, PositionMode, PositionMode},
+		[]ParameterMode{PositionMode, PositionMode, PositionMode, PositionMode},
+		[]ParameterMode{PositionMode, PositionMode, PositionMode, PositionMode},
+		[]ParameterMode{PositionMode, PositionMode, PositionMode, PositionMode}}
+
+	for i, input := range inputs {
+		code, modes := SplitOp(input)
+
+		if code != expectedOps[i] {
+			t.Errorf("incorrect op code %v for input %v; expected %v", code, input, expectedOps[i])
+		}
+
+		if len(modes) != len(expectedMod[i]) {
+			t.Errorf("incorrect parameter length %v; expected %v", len(modes), len(expectedMod[i]))
+		} else {
+			for j, mod := range expectedMod[i] {
+				if modes[j] != mod {
+					t.Errorf("incorrect mode value %v; expected %v", modes[j], mod)
+				}
+			}
+		}
+	}
+}
+
+func TestGetParameters(t *testing.T) {
+	inputs := [][]int{
+		[]int{2, 3, 1, 1, 99},
+		[]int{3, 4, 1, 4, 3, 3, 99},
+		[]int{5, 6, 4, 1, 4, 3, 3, 99},
+		[]int{3, 0, 4, 0, 99}}
+
+	index := []int{1, 3, 1, 3}
+	length := []int{3, 3, 2, 1}
+	expected := [][]int{
+		[]int{3, 1, 1},
+		[]int{4, 3, 3},
+		[]int{6, 4},
+		[]int{0}}
+
+	for i, input := range inputs {
+		p := GetParameters(length[i], index[i], input)
+
+		for j, b := range expected[i] {
+			if p[j].Value != b {
+				t.Errorf("incorrect parameter %v; expected %v", p[j].Value, b)
+			}
+		}
+	}
+}
+
+func TestGetInstruction_Modes(t *testing.T) {
+	inputs := [][]int{
+		[]int{1002, 0, 2, 0, 99},
+		[]int{3, 3, 11101, 3, 4, 0, 99},
+		[]int{3, 0, 4, 0, 99},
+		[]int{3, 3, 1105, 4, 9, 1101, 0, 0, 12, 4, 12, 99, 1}}
+
+	inputInd := []int{0, 2, 0, 2}
+	expectedMod := [][]ParameterMode{
+		[]ParameterMode{PositionMode, ImmediateMode, PositionMode, PositionMode},
+		[]ParameterMode{ImmediateMode, ImmediateMode, ImmediateMode, PositionMode},
+		[]ParameterMode{PositionMode, PositionMode, PositionMode, PositionMode},
+		[]ParameterMode{ImmediateMode, ImmediateMode, PositionMode, PositionMode}}
+
+	for i, input := range inputs {
+		inst := GetInstruction(inputInd[i], input)
+
+		if len(inst.Modes) != len(expectedMod[i]) {
+			t.Errorf("incorrect parameter length %v; expected %v", len(inst.Modes), len(expectedMod[i]))
+		} else {
+			for j, mod := range expectedMod[i] {
+				if inst.Modes[j] != mod {
+					t.Errorf("incorrect mode value %v; expected %v", inst.Modes[j], mod)
+				}
+			}
+		}
+	}
+}
+
+func TestGetInstruction_Parameter(t *testing.T) {
+	inputs := [][]int{
+		[]int{1002, 0, 2, 0, 99},
+		[]int{3, 3, 11101, 3, 4, 0, 99},
+		[]int{3, 0, 99},
+		[]int{4, 0, 99},
+		[]int{5, 0, 2, 99},
+		[]int{6, 0, 2, 99},
+		[]int{7, 0, 2, 99},
+		[]int{8, 0, 2, 99}}
+
+	inputInd := []int{0, 2, 0, 0, 0, 0, 0, 0}
+	expected := []int{3, 3, 1, 1, 2, 2, 3, 3}
+
+	for i, input := range inputs {
+		inst := GetInstruction(inputInd[i], input)
+
+		if len(inst.Parameters) != expected[i] {
+			t.Errorf("incorrect number of parameters %v; expected %v", len(inst.Parameters), expected[i])
+		}
+	}
+}
+
+func TestGetInstruction(t *testing.T) {
+	inputs := [][]int{
+		[]int{1002, 0, 2, 0, 99},
+		[]int{3, 225, 4, 225},
+		[]int{4, 225, 1, 225}}
+	expected := []Instruction{
+		Instruction{
+			Position: 0,
+			Op:       MultiplyOp,
+			Parameters: []Parameter{
+				Parameter{Value: 0},
+				Parameter{Value: 2},
+				Parameter{Value: 0}},
+			Modes: []ParameterMode{
+				PositionMode,
+				ImmediateMode,
+				PositionMode}},
+		Instruction{
+			Position: 2,
+			Op:       InputOp,
+			Parameters: []Parameter{
+				Parameter{Value: 225, Position: 3}},
+			Modes: []ParameterMode{
+				PositionMode}},
+		Instruction{
+			Position: 0,
+			Op:       OutputOp,
+			Parameters: []Parameter{
+				Parameter{Value: 225}},
+			Modes: []ParameterMode{
+				PositionMode}}}
+
+	expectedI := []int{4, 2, 2}
+
+	for i, input := range inputs {
+		inst := GetInstruction(0, input)
+
+		if inst.NextInstruction() != expectedI[i] {
+			t.Errorf("incorrect next position %v; expected %v", inst.NextInstruction(), expectedI[i])
+		}
+
+		if inst.Op != expected[i].Op {
+			t.Errorf("incorrect op code %v; expected %v", inst.Op, expected[i].Op)
+		}
+
+		if len(inst.Parameters) != len(expected[i].Parameters) {
+			t.Errorf("incorrect parameter length %v; expected %v", len(inst.Parameters), len(expected[i].Parameters))
+		}
+
+		for j, p := range inst.Parameters {
+			if p.Value != expected[i].Parameters[j].Value {
+				t.Errorf("incorrect parameter %v; expected %v", p.Value, expected[i].Parameters[j].Value)
+			}
+
+			if inst.Modes[j] != expected[i].Modes[j] {
+				t.Errorf("incorrect mode %v; expected %v", inst.Modes[j], expected[i].Modes[j])
+			}
+		}
+	}
+}
+
+func TestFullIntegration(t *testing.T) {
+	inputs := [][]int{
+		[]int{3, 0, 4, 0, 99},
+		[]int{3, 0, 1, 0, 0, 0, 4, 0, 99},
+		[]int{3, 0, 2, 0, 0, 0, 4, 0, 99},
+		[]int{3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8},
+		[]int{3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8},
+		[]int{3, 3, 1108, -1, 8, 3, 4, 3, 99},
+		[]int{3, 3, 1107, -1, 8, 3, 4, 3, 99},
+		[]int{3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9},
+		[]int{3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1},
+		[]int{3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99}}
+	expected := []string{"4", "8", "16", "0", "1", "0", "1", "1", "1", "999"}
+
+	for i, set := range inputs {
+		position := 0
+		reader := bytes.NewReader([]byte{'4', '\n'})
+		writer := bytes.NewBuffer([]byte{})
+		instruction := GetInstruction(position, set)
+		position = ExecInstruction(reader, writer, instruction, set)
+		if position >= 0 {
+			Process(reader, writer, position, set)
+		}
+
+		result := strings.Split(strings.TrimRight(fmt.Sprintf("%v", writer), "\n"), "\n")
+		if result[len(result)-1] != expected[i] {
+			t.Errorf("incorrect result %v for test %v; expected %v", result[len(result)-1], i+1, expected[i])
+		}
+	}
+}
